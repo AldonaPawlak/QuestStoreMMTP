fc7693eb2b70f506d2ad9487664526b72be268a1
Marta Skarbek <marta@skarbek.name>
2020-07-09 10:08:24 +0200

    changed structure of files

---
+++ Public/script.js
@@ -0,0 +1,91 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "confirm pass";
+}
+
+function showSignInForm() {
+    const confirmPassword = document.querySelector('.container > input');
+    confirmPassword.style.display = 'block';
+}
+
+const signIn = document.querySelector('.container > button');
+signIn.addEventListener("click", showSignInForm)
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+    // document.getElementById('content').style.marginLeft = '258px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    // document.getElementById('content').style.marginLeft = '8px';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+    // document.getElementById('content').style.marginLeft = '258px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    // document.getElementById('content').style.marginLeft = '8px';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+    // document.getElementById('content').style.marginLeft = '258px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    // document.getElementById('content').style.marginLeft = '8px';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+    // document.getElementById('content').style.marginLeft = '258px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    // document.getElementById('content').style.marginLeft = '8px';
+}
+
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+}
