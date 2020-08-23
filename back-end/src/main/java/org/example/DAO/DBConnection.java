bc6ccace81d01aa2dd7c4c7d0eb294f06d414bad
Marta Skarbek <marta@skarbek.name>
2020-08-23 20:01:45 +0200

    added basic DAO implementation

---
+++ back-end/src/main/java/org/example/DAO/DBConnection.java
@@ -0,0 +1,40 @@
+/*
+package org.example.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Connection {
+
+*/
+/*    public void a() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/test",
+                            "test", "lalala");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "insert into bla (id, name) VALUES (423, 'akjshngfjehwg');";
+            *//*
+*/
+/*  String sql = "insert into bla (id, name) values (1, 'Marta')";*//*
+*/
+/*
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }*//*
+
+
+}
+*/
