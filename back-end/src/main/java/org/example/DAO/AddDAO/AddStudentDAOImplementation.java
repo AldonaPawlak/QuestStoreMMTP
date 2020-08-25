c693c9f458c095937d9b7d18519fc580a476222c
Marta Skarbek <marta@skarbek.name>
2020-08-25 10:47:40 +0200

    fixed addStudentDaoImplementation path to student

--- back-end/src/main/java/org/example/DAO/AddDAO/AddStudentDAOImplementation.java
+++ back-end/src/main/java/org/example/DAO/AddDAO/AddStudentDAOImplementation.java
@@ -2,12 +2,12 @@ package org.example.DAO.AddDAO;
 
 import org.example.DAO.DBConnection;
 
-public class AddStudentDAOImplementation<Student> implements AddDAO<Student> {
+public class AddStudentDAOImplementation<Student> implements AddDAO<org.example.model.Student> {
 
     DBConnection dbConnection = new DBConnection();
 
     @Override
-    public void add(Student student) {
+    public void add(org.example.model.Student student) {
          dbConnection.ExecuteStatement(String.format("INSERT into user_details (name, surname, email, password, role_id, is_active)" + "values (%s, %s ,%s ,%s, %d, %b);", student.getName()));
     }
 
