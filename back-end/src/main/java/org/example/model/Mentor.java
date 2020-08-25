7b569299ffc30a15dbddc2920c5d47274e03ed63
Marta Skarbek <marta@skarbek.name>
2020-08-25 13:34:15 +0200

    added adding artifacts

--- back-end/src/main/java/org/example/model/Mentor.java
+++ back-end/src/main/java/org/example/model/Mentor.java
@@ -6,8 +6,9 @@ public class Mentor extends User {
 
     private UUID mentorID;
 
-    public Mentor(UUID id, String name, String surname, String email, String password, UUID roleID, UUID mentorID, boolean isActive) {
+    public Mentor(UUID id, String name, String surname, String email, String password, UUID roleID, boolean isActive, UUID mentorID) {
         super(id, name, surname, email, password, roleID, isActive);
         this.mentorID = mentorID;
     }
+
 }
