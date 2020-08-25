dbc70a192ad45c46fe77722888b2d3ef9c358e23
Marta Skarbek <marta@skarbek.name>
2020-08-25 19:35:13 +0200

    refactored add dao functions

--- back-end/src/main/java/org/example/DAO/AddDAO/AddMentorDAOImplementation.java
+++ back-end/src/main/java/org/example/DAO/AddDAO/AddMentorDAOImplementation.java
@@ -1,13 +1,14 @@
 package org.example.DAO.AddDAO;
 
 import org.example.DAO.DBConnection;
+import org.example.model.Mentor;
 
-public class AddMentorDAOImplementation implements AddDAO<org.example.model.Mentor> {
+public class AddMentorDAOImplementation implements AddDAO<Mentor> {
 
     DBConnection dbConnection = new DBConnection();
 
     @Override
-    public void add(org.example.model.Mentor mentor) {
+    public void add(Mentor mentor) {
         dbConnection.ExecuteStatement(String.format("INSERT INTO user_details (id, name, surname, email, password, role_id, is_active) VALUES (%s, %s ,%s ,%s, %s, 2, true);", mentor.getId(), mentor.getName(), mentor.getSurname(), mentor.getEmail(), mentor.getPassword()));
         dbConnection.ExecuteStatement(String.format("INSERT INTO mentors (id, user_details_id) VALUES (%s, %s);", mentor.getMentorID(), mentor.getId()));
     }
