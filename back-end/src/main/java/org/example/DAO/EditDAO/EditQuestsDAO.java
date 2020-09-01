a3bbcc600be77d9b4f368d6848a1048ee8ac45db
Tomek K <tomek.krogulec@o2.pl>
2020-09-01 11:22:24 +0200

    add EditQuestsDAO

---
+++ back-end/src/main/java/org/example/DAO/EditDAO/EditQuestsDAO.java
@@ -0,0 +1,18 @@
+package org.example.DAO.EditDAO;
+
+import org.example.DAO.DBConnection;
+import org.example.model.Quest;
+
+public class EditQuestsDAO implements EditDAO<Quest> {
+
+    DBConnection dbConnection;
+
+    public EditQuestsDAO(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    @Override
+    public void edit(Quest quest) {
+        dbConnection.ExecuteStatement(String.format("UPDATE quests SET name = %s, description = %s, value = %d WHERE id = %s;", quest.getName(), quest.getDescription(), quest.getValue(), quest.getId()));
+    }
+}
