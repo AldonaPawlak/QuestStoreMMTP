fb474aefff1780da942e4210369a45f164b80391
Marta Skarbek <marta@skarbek.name>
2020-09-02 04:51:55 +0200

    refactored DAO

---
+++ back-end/src/main/java/org/example/DAO/ArtifactDAOImplementation.java
@@ -0,0 +1,77 @@
+package org.example.DAO;
+
+import org.example.model.Artifact;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class ArtifactDAOImplementation implements DAO<Artifact>{
+
+    DBConnection dbConnection;
+    DAOGetSet daoGetSet;
+
+    public ArtifactDAOImplementation(DBConnection dbConnection, DAOGetSet daoGetSet) {
+        this.dbConnection = dbConnection;
+        this.daoGetSet = daoGetSet;
+    }
+
+
+    @Override
+    public void add(Artifact artifact) {
+        dbConnection.ExecuteStatement(String.format("INSERT INTO artifacts (id, name, price, category_id, description, artifact_type_id) VALUES('%s', '%s', '%d', '%s', '%s', '%s');", artifact.getId(), artifact.getName(), artifact.getCategoryID(), artifact.getDescription(), artifact.getArtifactTypeID()));
+    }
+
+    @Override
+    public void remove(Artifact artifact) {
+        dbConnection.ExecuteStatement(String.format("DELETE FROM artifacts WHERE id = '%s';", artifact.getId()));
+    }
+
+    @Override
+    public void edit(Artifact artifact) {
+        dbConnection.ExecuteStatement(String.format("UPDATE artifacts SET name = '%s', price = %d, category_id = '%s', description = '%s', artifact_type_id = '%s' WHERE id = '%s';", artifact.getName(), artifact.getPrice(), artifact.getCategoryID(), artifact.getDescription(), artifact.getArtifactTypeID(), artifact.getId()));
+    }
+
+    @Override
+    public List<Artifact> getAll() {
+        List<Artifact> artifacts = new ArrayList<>();
+        try {
+            ResultSet allArtifacts = daoGetSet.getDataSet("SELECT * FROM artifacts;");
+            while (allArtifacts.next()) {
+                final UUID id = UUID.fromString(allArtifacts.getString("id"));
+                final String name = allArtifacts.getString("name");
+                final int price = allArtifacts.getInt("price");
+                final UUID categoryID = UUID.fromString(allArtifacts.getString("category_id"));
+                final String description = allArtifacts.getString("description");
+                final UUID artifactTypeID = UUID.fromString(allArtifacts.getString("artifact_type_id"));
+                Artifact artifact = new Artifact(id, name, price, categoryID, description, artifactTypeID);
+                artifacts.add(artifact);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return artifacts;
+    }
+
+    @Override
+    public Artifact get(UUID id) {
+        Artifact artifact;
+        try {
+            ResultSet allArtifacts = daoGetSet.getDataSet(String.format("SELECT * FROM artifacts WHERE id='%s';", id.toString()));
+            while (allArtifacts.next()) {
+                final UUID artifactID = UUID.fromString(allArtifacts.getString("id"));
+                final String name = allArtifacts.getString("name");
+                final int price = allArtifacts.getInt("price");
+                final UUID categoryID = UUID.fromString(allArtifacts.getString("category_id"));
+                final String description = allArtifacts.getString("description");
+                final UUID artifactTypeID = UUID.fromString(allArtifacts.getString("artifact_type_id"));
+                artifact = new Artifact(artifactID, name, price, categoryID, description, artifactTypeID);
+                return artifact;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ back-end/src/main/java/org/example/DAO/MentorDAOImplementation.java
@@ -0,0 +1,46 @@
+package org.example.DAO;
+
+import org.example.model.Mentor;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+public class MentorDAOImplementation implements DAO<Mentor> {
+
+    DBConnection dbConnection;
+    DAOGetSet daoGetSet;
+
+    public MentorDAOImplementation(DBConnection dbConnection, DAOGetSet daoGetSet) {
+        this.dbConnection = dbConnection;
+        this.daoGetSet = daoGetSet;
+    }
+
+    @Override
+    public void add(Mentor mentor) {
+        dbConnection.ExecuteStatement(String.format("INSERT INTO user_details (id, name, surname, email, password, role_id, is_active) VALUES ('%s', '%s' ,'%s' ,'%s', '%s', 2, true);", mentor.getUserDetailsID(), mentor.getName(), mentor.getSurname(), mentor.getEmail(), mentor.getPassword()));
+        dbConnection.ExecuteStatement(String.format("INSERT INTO mentors (id, user_details_id) VALUES ('%s', '%s');", mentor.getMentorID(), mentor.getUserDetailsID()));
+    }
+
+    @Override
+    public void remove(Mentor mentor) {
+        dbConnection.ExecuteStatement(String.format("DELETE FROM mentors '%s';", mentor.getMentorID()));
+        dbConnection.ExecuteStatement(String.format("DELETE FROM user_details WHERE id = '%s';", mentor.getUserDetailsID()));
+    }
+
+    @Override
+    public void edit(Mentor mentor) {
+        dbConnection.ExecuteStatement(String.format("UPDATE mentors SET user_details_id = '%s' WHERE id = '%s';", mentor.getUserDetailsID(), mentor.getMentorID()));
+    }
+
+    @Override
+    public List<Mentor> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Mentor get(UUID id) {
+        return null;
+    }
+
+}
