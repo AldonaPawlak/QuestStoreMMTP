40b2884a7fc63ff501a7f5c6ebb80245c5d49eb7
Marta Skarbek <marta@skarbek.name>
2020-09-16 17:45:30 +0200

    login js addition

--- back-end/src/Public/login.js
+++ back-end/src/Public/login.js
@@ -15,22 +15,20 @@ function login(data) {
             body: data
         })
         .then((response) => {
-            /*console.log(response)*/
             return response.json()
         })
         .then((responseJson) => {
-            console.log(responseJson);
             document.cookie = "user=" + responseJson.id + " " + responseJson.email + " " +responseJson.role;
             return responseJson;
         })
         .then ((responseJson) => {
             if (responseJson.role === "Student") {
-                window.location.href = "http://localhost:63342/back-end/src/Public/websiteLogic/students.html?_ijt=q648jnvbkhk07ins1joab5utnn";}
+                window.location.href = "students.html";}
             else if (responseJson.role === "Mentor") {
-                window.location.href = "http://localhost:63342/back-end/src/Public/websiteLogic/mentors.html?_ijt=ob378oqekqtl63nv30sgik9p3q";
+                window.location.href = "mentors.html";
             }
             else if (responseJson.role === "Creep") {
-                window.location.href = "http://localhost:63342/back-end/src/Public/websiteLogic/mentorView.html?_ijt=q648jnvbkhk07ins1joab5utnn";
+                window.location.href = "mentorView.html";
             }
         })
         .catch(error => console.error(error));
