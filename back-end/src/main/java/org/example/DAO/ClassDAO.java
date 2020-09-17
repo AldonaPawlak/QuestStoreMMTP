ba5801d6f8b6eb55ce6d1b26b00eba17ffe45e14
Marta Skarbek <marta@skarbek.name>
2020-09-17 05:32:31 +0200

    added selecting mentors and students classes

--- back-end/src/main/java/org/example/DAO/ClassDAO.java
+++ back-end/src/main/java/org/example/DAO/ClassDAO.java
@@ -2,7 +2,6 @@ package org.example.DAO;
 
 import org.example.DAO.Exception.AbsenceOfRecordsException;
 import org.example.model.Class;
-import org.example.model.Quest;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,7 +23,8 @@ public class ClassDAO implements DAO<Class>{
     public void add(Class claass) {
         try {
             dbConnection.connect();
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement("INSERT INTO classes (id, name) VALUES (?, ?);");
+            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(
+                    "INSERT INTO classes (id, name) VALUES (?, ?);");
             preparedStatement.setObject(1, claass.getId(), Types.OTHER);
             preparedStatement.setString(2, claass.getName());
             preparedStatement.executeUpdate();
@@ -40,7 +40,8 @@ public class ClassDAO implements DAO<Class>{
     public void remove(Class claass) {
         try {
             dbConnection.connect();
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement("DELETE FROM classes WHERE id = ?;");
+            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(
+                    "DELETE FROM classes WHERE id = ?;");
             preparedStatement.setObject(1, claass.getId(), Types.OTHER);
             preparedStatement.executeUpdate();
             dbConnection.disconnect();
@@ -55,7 +56,8 @@ public class ClassDAO implements DAO<Class>{
     public void edit(Class claass) {
         try {
             dbConnection.connect();
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement("UPDATE classes SET id = ?, name = ? WHERE id = ?;");
+            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(
+                    "UPDATE classes SET id = ?, name = ? WHERE id = ?;");
             preparedStatement.setObject(1, claass.getId(), Types.OTHER);
             preparedStatement.setString(2, claass.getName());
             preparedStatement.setObject(3, claass.getId());
@@ -73,7 +75,8 @@ public class ClassDAO implements DAO<Class>{
         List<Class> classes = new ArrayList<>();
         try {
             dbConnection.connect();
-            PreparedStatement preparedStatement = dbConnection.connect().prepareStatement("SELECT * FROM classes;");
+            PreparedStatement preparedStatement = dbConnection.connect().prepareStatement(
+                    "SELECT * FROM classes;");
             ResultSet classesSet = preparedStatement.executeQuery();
             while (classesSet.next()) {
                 final UUID id = UUID.fromString(classesSet.getString("id"));
@@ -111,6 +114,56 @@ public class ClassDAO implements DAO<Class>{
             System.out.println("Selecting class from data base failed.");
         }
         throw new AbsenceOfRecordsException();
+    }
+
+    public List<Class> getAllStudentClasses(UUID id) {
+        List<Class> classes = new ArrayList<>();
+        try {
+            dbConnection.connect();
+            PreparedStatement preparedStatement = dbConnection.connect().prepareStatement(
+                    " SELECT id, name FROM classes, students_classes " +
+                            "WHERE classes.id = students_classes.classes_id " +
+                            "AND students_classes.student_id= ?;");
+            preparedStatement.setObject(1, id, Types.OTHER);
+            ResultSet classesSet = preparedStatement.executeQuery();
+            while (classesSet.next()) {
+                final UUID classID = UUID.fromString(classesSet.getString("id"));
+                final String name = classesSet.getString("name");
+                Class claass = new Class(classID, name);
+                classes.add(claass);
+            }
+            dbConnection.disconnect();
+            System.out.println("Selected classes from data base successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Selecting classes from data base failed.");
+        }
+        return classes;
+    }
+
+    public List<Class> getAllMentorClasses(UUID id) {
+        List<Class> classes = new ArrayList<>();
+        try {
+            dbConnection.connect();
+            PreparedStatement preparedStatement = dbConnection.connect().prepareStatement(
+                    " SELECT id, name FROM classes, mentors_classes " +
+                            "WHERE classes.id = mentors_classes.classes_id " +
+                            "AND mentors_classes.mentor_id= ?;");
+            preparedStatement.setObject(1, id, Types.OTHER);
+            ResultSet classesSet = preparedStatement.executeQuery();
+            while (classesSet.next()) {
+                final UUID classID = UUID.fromString(classesSet.getString("id"));
+                final String name = classesSet.getString("name");
+                Class claass = new Class(classID, name);
+                classes.add(claass);
+            }
+            dbConnection.disconnect();
+            System.out.println("Selected classes from data base successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Selecting classes from data base failed.");
+        }
+        return classes;
     }
 
 }
