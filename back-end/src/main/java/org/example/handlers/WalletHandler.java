571c957fb27b725264fe0a68172979449ea30715
Patryk Krol <patryk.krol.98@gmail.com>
2020-09-18 09:15:44 +0200

    add WalletHandler

---
+++ back-end/src/main/java/org/example/handlers/WalletHandler.java
@@ -0,0 +1,83 @@
+package org.example.handlers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.example.DAO.ArtifactDAO;
+import org.example.DAO.DBConnection;
+import org.example.DAO.Exception.AbsenceOfRecordsException;
+import org.example.DAO.StudentDAO;
+import org.example.model.Artifact;
+import org.example.model.Student;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class WalletHandler implements HttpHandler {
+
+    DBConnection dbConnection;
+    StudentDAO studentDAO;
+    ArtifactDAO artifactDAO;
+
+    public WalletHandler() {
+        this.dbConnection = new DBConnection();
+        this.artifactDAO = new ArtifactDAO(dbConnection);
+        this.studentDAO = new StudentDAO(dbConnection);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().put("Access-Control-Allow-Methods", Collections.singletonList("*"));
+        exchange.getResponseHeaders().put("Content-type", Collections.singletonList("application/json"));
+        exchange.getResponseHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
+        String method = exchange.getRequestMethod();
+        String response = "";
+        System.out.println("Method " + method);
+        int status = 200;
+        try {
+            if (method.equals("GET")) {
+                String url = exchange.getRequestURI().getRawPath();
+                String[] urlParts = url.split("/");
+                String userDetailsID = urlParts[2];
+                if (urlParts[3].equals("balance")) {
+                    response = getBalance(userDetailsID);
+                }
+                if (urlParts[3].equals("cards")) {
+                    response = getArtifacts(userDetailsID);
+                }
+                System.out.println(response);
+                sendResponse(response, exchange, status);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert response != null;
+            exchange.sendResponseHeaders(404, response.getBytes().length);
+        }
+    }
+
+    private String getArtifacts(String userDetailsID) throws JsonProcessingException {
+        List<Artifact> artifacts = artifactDAO.getAllbyID(UUID.fromString(userDetailsID));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(artifacts);
+    }
+
+    private String getBalance(String id) throws AbsenceOfRecordsException {
+        Student student = studentDAO.get(UUID.fromString(id));
+        return String.valueOf(student.getCoins());
+    }
+
+    private void sendResponse(String response, HttpExchange exchange, int status) throws IOException {
+        if (status == 200) {
+            exchange.getResponseHeaders().put("Content-type", Collections.singletonList("application/json"));
+            exchange.getResponseHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
+        }
+        exchange.sendResponseHeaders(status, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
