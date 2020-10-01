2556a046374bd2d653a8520a3d8b5a065b393151
wtokarz80 <w.tokarz80gmail.com>
2020-10-01 20:26:54 +0200

    add tests to ArtifactHanler class

--- back-end/src/main/java/org/example/App.java
+++ back-end/src/main/java/org/example/App.java
@@ -28,7 +28,7 @@ public class App
         server.createContext("/student", new StudentHandler(studentDAO, userDAO, mapper));
         server.createContext("/mentorView", new ProfileHandler(dbConnection, userDAO));
         server.createContext("/login", new LoginHandler(dbConnection, loginDAO));
-        server.createContext("/shop", new ArtifactHandler(dbConnection, artifactDAO));
+        server.createContext("/shop", new ArtifactHandler(artifactDAO, mapper));
         server.createContext("/quest", new QuestHandler(questDAO, mapper));
         server.createContext("/wallet", new WalletHandler(dbConnection, studentDAO, artifactDAO));
 
