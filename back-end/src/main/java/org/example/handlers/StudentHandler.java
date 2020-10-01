fbd01e2dea904cd039a6dc83eb740fcd8e7bb10b
wtokarz80 <w.tokarz80gmail.com>
2020-10-01 11:04:02 +0200

    code refactor

--- back-end/src/main/java/org/example/handlers/StudentHandler.java
+++ back-end/src/main/java/org/example/handlers/StudentHandler.java
@@ -125,3 +125,4 @@ public class StudentHandler implements HttpHandler {
     }
 
 }
+
--- back-end/src/test/java/org/example/handlers/StudentHandlerTest2.java
+++ back-end/src/test/java/org/example/handlers/StudentHandlerTest2.java
@@ -2,7 +2,6 @@ package org.example.handlers;
 
 import org.example.DAO.StudentDAO;
 import org.example.DAO.UserDAO;
-import org.example.config.PasswordCrypter;
 import org.example.model.Student;
 import org.example.model.User;
 
@@ -15,9 +14,7 @@ import org.junit.jupiter.api.Test;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -62,31 +59,4 @@ public class StudentHandlerTest2 {
         return students;
     }
 
-    @Test
-    public void should_add_student(){
-        //given
-
-//        List<User> students = Arrays.asList(new Student("Ala", "ala@ala.pl"), new Student("Ola", "ola@ola.pl"));
-        User user = new Student("Tomek", "tom@tom.pl");
-        doNothing().when(userDAO).add(user);
-
-
-
-        //when
-
-
-        //then
-
-
-
-    }
-
-
-
 }
-//    void addStudent() {
-//        User user = new Student(UUID.randomUUID(), "Name", "Surname", "mail@mail.com",
-//                PasswordCrypter.crypter("password"), UUID.fromString("745792a7-681b-4efe-abdd-ca027678b397"),
-//                true, "444 222 000", "student", UUID.randomUUID(), 0);
-//        userDAO.add(user);
-//    }
